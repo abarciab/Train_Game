2f3e3e3b11cbb54ff338fde116b27ed7aaff9bf4
Manoj Sara <msara@ucsc.edu>
2022-04-18 11:24:18 -0700

    added basic train code

--- src/scenes/PlayGame.js
+++ src/scenes/PlayGame.js
@@ -3,8 +3,13 @@ class PlayGame extends Phaser.Scene {
         super("playGame");
     }
 
-    create() {
+    preload() {
+        // load images/tile sprites
+        this.load.image('train', './assets/train.png');
+    }
 
+    create() {
+        this.train = new Train(this, game.config.width/2, game.config.height - borderUISize, 'train').setOrigin(0,0);
     }
 
     update(time, delta) {
