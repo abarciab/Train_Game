54b468555e69e10f8eb3deac1ee525115457382a
Andrew Wei <weiandrew2012@gmail.com>
2022-04-19 15:02:28 -0700

    commit

--- src/scenes/PlayGame.js
+++ src/scenes/PlayGame.js
@@ -17,6 +17,8 @@ class PlayGame extends Phaser.Scene {
         D_key = this.input.keyboard.addKey('D');
 
         let num_tracks = 3;
+        this.dx; // delta x; how much the player has traveled
+        this.j_tick = config.width / 4;
         this.tracks = {}; // key: track row, value: track objects
         this.junctions = {}; // key: track row, value: junction objects
         for (let i = 1; i <= num_tracks; i++) {
@@ -27,11 +29,15 @@ class PlayGame extends Phaser.Scene {
     }
 
     update(time, delta) {
+        this.updateTracks(delta);
         this.updateEvents(delta);
     }
 
     updateTracks(delta) {
-
+        if (this.dx >= this.j_tick) {
+            this.dx = 0;
+            SpawnTracks();
+        }
     }
 
     updateEvents(delta) {
