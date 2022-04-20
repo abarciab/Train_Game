273726b84507eb5f88849eb50f3d1fe00c844169
Andrew Wei <weiandrew2012@gmail.com>
2022-04-19 18:19:32 -0700

    pull

--- src/GenerateWorld.js
+++ src/GenerateWorld.js
@@ -7,9 +7,11 @@ function testMethod() {
     - more specifically, place down a node track at that point consistently
     - at x distance before a node track, can choose which direction you want to go
         - if no input, go straight.
+    - each node has a chance of having an obstacle in front of it as well
 2.) obstacles: can't spawn on junctions. so low chance to spawn btwn ticks, or make new ticks/offsets for it
 3.) stations (?)
 */
+
 // 1920 / 4: distance of ticks
 // spawn a chunk of tracks (one tick to another tick)
 function SpawnTracks(tracks, player_y) {
