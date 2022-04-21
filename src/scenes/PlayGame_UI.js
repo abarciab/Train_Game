4b8395a29b2a874f5413093fa1ed36a425f15626
LucianRain <aidanbacon@gmail.com>
2022-04-21 15:58:35 -0700

    temp UI commit

--- src/scenes/PlayGame_UI.js
+++ src/scenes/PlayGame_UI.js
@@ -19,25 +19,105 @@ this.textConfig = {
 
 function LoadUI(scene){
     scene.load.image('biome_bar', './assets/UI/biome bar.png');
+    scene.load.image('passenger_UI', './assets/UI/passenger UI.png');
 
-    //console.log("loaded all the assets required to display UI");
+    scene.load.image('mask', './assets/UI/circle mask test.png');
+
+    scene.load.image('pass_tri', './assets/UI/passenger triangle.png');
+    scene.load.image('pass_circle', './assets/UI/passenger circle.png');
+    scene.load.image('pass_square', './assets/UI/passenger square.png');
+
+    //scene.load.image('patience_bar', './assets/UI/passenger patience.png');
+    scene.load.spritesheet('patience_bar', './assets/UI/passenger patience.png', { frameWidth: 60, frameHeight: 60, startFrame: 0, endFrame: 4 });
+   
 }
 
 function StartUI(scene){
     //console.log("started UI Display");
     //let topBar;
+    this.front = 200;
+    this.IconGap = 80;
+    this.iconScale = 0.5;
+    this.bottomBarYpos = game.config.height - 60;
+
+    scene.anims.create({
+        key: 'patience_bar_anim',
+        frames: scene.anims.generateFrameNumbers('patience_bar', { start: 0, end: 4, first: 0 }),
+        frameRate: 3,
+        repeat: 0,
+    });
+
+    this.numPassengers = 0;
+    let groupConfig = {
+        classType: PassengerIcon
+    }
+    this.passengers = scene.add.group(groupConfig);
+
     this.topBar = scene.add.rectangle(game.config.width/2, 60, game.config.width*0.8, 80, 0xFFFFFF).setOrigin(0.5);
-    this.bottom = scene.add.rectangle(game.config.width/2, game.config.height - 60, game.config.width*0.8, 80, 0xFFFFFF).setOrigin(0.5);
+    this.bottomBar = scene.add.rectangle(game.config.width/2, this.bottomBarYpos, game.config.width*0.8, 80, 0xFF00FF).setOrigin(0.5);
 
-    //scene.add.image(game.config.width/4, game.config.height/2, 'basic_out-up_track').setOrigin(0.5).setDepth(10);
-    //scene.add.image(200, game.config.height/2, 'WIP_station').setOrigin(0.5).setDepth(10).setScale(scene.global_scaling);
+    //this.passengerTriangle = scene.add.image(this.front, game.config.height-60, 'pass_tri').setOrigin(0.5);
+    //console.log("x: " + this.passengerTriangle.x + " y: " + this.passengerTriangle.y);
+    //this.passengerCircle = scene.add.image(this.front + this.IconGap, game.config.height-60, 'pass_circle').setOrigin(0.5);
+    //this.passengerSquare = scene.add.image(this.front + this.IconGap*2, game.config.height-60, 'pass_square').setOrigin(0.5);
 
-
+    addPasengerUI(scene, 'pass_tri', 2000);
+    addPasengerUI(scene, 'pass_tri', 2000);
+    addPasengerUI(scene, 'pass_square', 2000);
+    addPasengerUI(scene, 'pass_tri', 2000);
+    addPasengerUI(scene, 'pass_circle', 2000);
+    
     this.dist = scene.add.text(game.config.width*0.8, this.topBar.y, "Dist: 20,000m", this.textConfig).setOrigin(0.5);
     this.biomeBar = scene.add.image(game.config.width/2, this.topBar.y, 'biome_bar');
+
 }
 
-function UpdateUI(scene){
-    console.log("udpated UI Display");
+function UpdateUI(scene, delta){
+    //console.log("udpated UI Display: delata: " + delta);
+
+    this.passengers.children.iterate((passenger) => {
+        if (passenger.patience == 2000){
+            
+
+        }
+
+        passenger.updatePatience(delta);
+    });
+
 }
 
+function addPasengerUI(scene, shape, patience){
+    this.numPassengers += 1;
+
+    console.log("added a passenger");
+
+    this.newPassIcon = new PassengerIcon(scene, this.front + (IconGap*numPassengers), bottomBarYpos, shape, patience).setScale(this.iconScale);
+
+    this.passengers.add(newPassIcon);
+}
+
+class PassengerIcon extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, patience) {
+        super(scene, x, y, texture);
+
+        this.setOrigin(0.5);
+        scene.add.existing(this);
+
+        this.patienceBar = scene.add.sprite(x, y + 20, 'patience_bar');
+
+        this.patience = patience;
+        console.log("added a passenger with " + patience + " patience");
+
+        //this.patienceBar.anims.play('patience_bar_anim');
+    }
+
+    updatePatience(delta){
+        if (this.patience > 0){
+            this.patience -= delta;
+            this.patienceBar.setScale(this.patienceBar.scaleX * 0.99, 1);
+            if (this.patience <= 0){
+                console.log("PASSENGER HAS RUN OUT OF PATIENCE!!!");
+            }
+        }
+    }
+}
