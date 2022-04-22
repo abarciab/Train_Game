bbccfc2db48489069ef2d7bb7b3f6cf48973320a
ManojSara <msara@ucsc.edu>
2022-04-21 17:05:48 -0700

    edited passenger and train vars

--- src/prefabs/Passenger.js
+++ src/prefabs/Passenger.js
@@ -6,7 +6,6 @@ class Passenger extends Phaser.GameObjects.Sprite {
         this.destination == station;   // Station the passenger gets off at
         this.reached = false;     // Tracks if passenger reached destination
         this.onTrain = false;     // Tracks if passenger is on train
-        // this.junctionsPassed = 0; // # of junctions passed while in train
         this.goodReview = true;   // Tracks if passenger will give good review
         this.patience = patience; //the number of time this passenger is willing to wait on the train untill they get mad.
 
--- src/prefabs/Train.js
+++ src/prefabs/Train.js
@@ -9,6 +9,7 @@ class Train extends Phaser.GameObjects.Sprite {
         this.passengers = [];                 // list of passengers in train
         this.capacity = 6;                    // # of passengers the train can fit
         this.fuelCapacity = 10000;            // max amount of fuel Train can hold
+        this.distanceTraveled = 0;            // # of nodes passed
         this.displayOriginX = 200;
         this.junction_wid = (1184-128)*scaling;
         this.turning = false;
